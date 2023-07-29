8a7594d57ffd0c47428ff122358564e4b40e7dfb
Khushi Patel <khushipatel3340@gmail.com>
2023-07-29 16:39:37 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,83 @@
-import React from "react";
+import { useEffect, useState } from "react"
+import "./styles.css"
 
-function App() {
-  return (
-    <>
-      <h1>Todo List</h1>
-      <h2>What needs to be done?</h2>
-      <div>
-        <input type="text"/>
-        <button>Add</button>
-      </div>
-      <div>
-        <button>Show all tasks</button>
-        <button>Show active tasks</button>
-        <button>Show completed tasks</button>
-      </div>
-      <h2>3 tasks remaining</h2>
-      <li>
-        <input type="checkbox"/>
-        Eat
-        <div>
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>
-      </li>
-      <li>
-        <input type="checkbox"/>
-        Sleep
-        <div>
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>
-      </li>
-      <li>
-      <input type="checkbox"/>
-        Repeat
-        <div>
-          <button>Edit</button>
-          <button>Delete</button>
-        </div>
-      </li>
-    </>
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue === null) return []
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (newItem === "") {
+      alert("You must write something!!")
+    } else {
+      setTodos((currentTodos) => {
+        if (newItem === "") {
+          alert("Cannot add empty item")
+          return [
+            ...currentTodos
+          ]
+        } else {
+          return [
+          ...currentTodos,
+          {
+            id: crypto.randomUUID(),
+            title: newItem,
+            completed: false
+          }
+          ]  
+        }
+      })
+      setNewItem("")
+    }
+  } 
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return ( 
+    <div className="main-div">  
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <h1>Todo List</h1>
+        <input value={newItem} onChange={e => setNewItem(e.target.value)}
+        type="text" />
+        <button className="btn">Add Item</button>
+      </form> 
+      <ul className="list">
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+              {todo.title}
+            </label>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+          </li>
+          )
+        })}
+    </ul>  
+    </div>
   )
 }
-
-export default App
